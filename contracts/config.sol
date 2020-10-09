13634749b972313198ad685d4e6eea36bc7bf118
xcpooo <50356818+xcpooo@users.noreply.github.com>
2020-10-09 13:51:34 +0800

    全局设置，方便调试

---
+++ contracts/config.sol
@@ -0,0 +1,11 @@
+contract config{
+    address config_token_address=0xb93fb4922095fb2f3ef0f249207e296bdc199f22;
+    address config_credit_token_address=0xfbfe6cba546b21e8752dfa252623e4aa7b96cede;
+    address config_sponsor_address=0x941a49f176d2b5f1e619cbf5aef9d52cb63452d6;
+    address config_tax_address=0xfbfe6cba546b21e8752dfa252623e4aa7b96cede;
+
+    string config_tax_table="Tax_table";
+    string config_supervise_table="Supervise_table";
+    string config_order_table="Order_table";
+    string config_waste_table="Waste_table";
+}
